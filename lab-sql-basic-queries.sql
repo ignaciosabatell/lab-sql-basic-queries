13a7f2f682b05d9b9491daf56dcbf2795e9b4858
ignaciosabatell <ignaciosabatel@gmail.com>
2025-10-31 18:01:31 +0100

    Add files via upload

---
+++ lab-sql-basic-queries.sql
@@ -0,0 +1,86 @@
+-- -- Display all available tables in the Sakila database.
+use sakila;
+
+-- Retrieve all the data from the tables actor, film and customer.
+
+SHOW TABLES;
+
+SELECT *
+FROM actor;
+
+SELECT *
+FROM film;
+
+SELECT *
+FROM customer;
+
+-- 3. Retrieve the following columns from their respective tables:
+-- 3.1 Titles of all films from the film table
+
+SELECT title
+FROM film;
+
+-- 3.2 List of languages used in films, with the column aliased as language from the language table
+
+SELECT name
+FROM language;
+
+-- 3.3 List of first names of all employees from the staff table.
+
+SELECT first_name
+FROM staff;
+
+-- 4.Retrieve unique release years.
+
+SELECT DISTINCT release_year
+FROM film;
+
+-- 5. Counting records for database insights:
+-- 5.1 Determine the number of stores that the company has.
+
+SELECT COUNT(DISTINCT store_id) AS number_of_stores
+FROM store;
+
+-- 5.2 Determine the number of employees that the company has.
+
+SELECT COUNT(DISTINCT staff_id) AS number_of_employees
+FROM staff;
+
+-- 5.3 Determine how many films are available for rent and how many have been rented.
+
+SELECT COUNT(DISTINCT inventory_id) AS total_films_available
+FROM inventory;
+
+SELECT COUNT(DISTINCT inventory_id) AS total_films_rented FROM rental;
+
+-- 5.4 Determine the number of distinct last names of the actors in the database.
+
+SELECT COUNT(DISTINCT last_name)
+FROM actor;
+
+-- 6. Retrieve the 10 longest films.
+SELECT title, length
+FROM film
+ORDER BY length DESC
+LIMIT 10;
+
+-- Use filtering techniques in order to:
+-- 7.1 Retrieve all actors with the first name "SCARLETT".
+
+SELECT *
+FROM actor
+WHERE first_name = 'SCARLETT';
+
+-- BONUS
+-- 7.2 Retrieve all movies that have ARMAGEDDON in their title and have a duration longer than 100 minutes.
+
+SELECT *
+FROM film
+WHERE title LIKE '%ARMAGEDDON%'
+  AND length > 100;
+
+
+
+
+
+
